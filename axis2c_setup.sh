93b636d34fa2c32a755388be41bbf7ec64909053
Dinesh Weerapurage <xydinesh@gmail.com>
2012-10-24 04:19:04 +0000

    add make

--- axis2c_setup.sh
+++ axis2c_setup.sh
@@ -1,2 +1,2 @@
- sudo yum install subversion apr apr-devel apr-util httpd httpd-devel emacs autoconf automake libxml2 libtool ibxml2-dev zlib zlib-devel
+ sudo yum install subversion apr apr-devel apr-util httpd httpd-devel emacs autoconf automake libxml2 libtool ibxml2-dev zlib zlib-devel make
  
