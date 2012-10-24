3a23b323075c94614cb870cb6e407acb9189a1a6
Dinesh Weerapurage <xydinesh@gmail.com>
2012-10-24 04:36:14 +0000

    set up axis2c env

--- axis2c_setup.sh
+++ axis2c_setup.sh
@@ -1,2 +1,4 @@
- sudo yum install subversion apr apr-devel apr-util httpd httpd-devel emacs autoconf automake libxml2 libtool ibxml2-dev zlib zlib-devel make
+git config --global user.name "Dinesh Weerapurage"
+git config --global user.email xydinesh@gmail.com
+sudo yum install subversion apr apr-devel apr-util httpd httpd-devel emacs autoconf automake libxml2 libtool ibxml2-dev zlib zlib-devel make
  
