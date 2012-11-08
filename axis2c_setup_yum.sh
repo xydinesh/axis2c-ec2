2483d0c57b5dd304ff85ef8b108f1abd734ac2aa
Dinesh Weerapurage <xydinesh@gmail.com>
2012-11-08 02:13:45 +0000

    add shell scripts for configure environment

---
+++ axis2c_setup_yum.sh
@@ -0,0 +1,4 @@
+git config --global user.name "Dinesh Weerapurage"
+git config --global user.email xydinesh@gmail.com
+sudo yum install subversion apr apr-devel apr-util httpd httpd-devel emacs autoconf automake libxml2 libtool ibxml2-dev zlib zlib-devel make
+ 
