ac890bd485e05eb828a08e405faca68670e1d37f
Dinesh Weerapurage <xydinesh@gmail.com>
2012-11-08 02:23:03 +0000

    added axis2c configuration (packages) scripts for ubuntu

--- axis2c_setup_apt.sh
+++ axis2c_setup_apt.sh
@@ -1,4 +1,2 @@
-git config --global user.name "Dinesh Weerapurage"
-git config --global user.email xydinesh@gmail.com
-sudo yum install subversion apr apr-devel apr-util httpd httpd-devel emacs autoconf automake libxml2 libtool ibxml2-dev zlib zlib-devel make
+sudo apt-get install make  subversion libapr1 libapr1-dev libaprutil1 emacs autoconf automake libxml2 libtool libxml2-dev zlibc apache2-mpm-worker
  
