b78bbfee7a2ccfbd2fbb3499e5739fcc3da5c4e8
Dinesh Weerapurage <xydinesh@gmail.com>
2013-01-03 04:19:12 +0000

    adding libcurl packages

--- axis2c_setup_apt.sh
+++ axis2c_setup_apt.sh
@@ -1,2 +1,2 @@
-sudo apt-get install make  subversion libapr1 libapr1-dev libaprutil1 emacs autoconf automake libxml2 libtool libxml2-dev zlibc apache2-mpm-worker pkg-config
+sudo apt-get install make  subversion libapr1 libapr1-dev libaprutil1 emacs autoconf automake libxml2 libtool libxml2-dev zlibc apache2-mpm-worker pkg-config libcurl4-openssl-dev
  
